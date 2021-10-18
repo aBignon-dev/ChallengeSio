3ca9cdac85140109302fffc15756bead17b9febc
Jiyno <Jean.andre.thomas22@gmail.com>
2021-10-18 15:50:46 +0200

    bouton indice pour Benoit

--- src/Controller/FlagController.php
+++ src/Controller/FlagController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FlagController extends AbstractController
 {
@@ -16,6 +17,19 @@ class FlagController extends AbstractController
         return $this->render('flag/index.html.twig', [
             'controller_name' => 'FlagController',
         ]);
+    }
+
+    public function redirection(): RedirectResponse
+    {
+    // redirects to the "homepage" route
+    return $this->redirectToRoute('homepage');
+
+    // redirectToRoute is a shortcut for:
+    // return new RedirectResponse($this->generateUrl('homepage'));
+
+    // redirects to a route and maintains the original query string parameters
+    //return $this->redirectToRoute('blog_show', $request->query->all());
+
     }
 
     
