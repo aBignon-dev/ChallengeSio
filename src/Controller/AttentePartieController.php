15b5a9174b5e4326e952ac207635fa0c13a22bc9
Sukando <abignon.ledantec@gmail.com>
2021-10-19 11:15:08 +0200

    Update AttentePartie affichage

--- src/Controller/AttentePartieController.php
+++ src/Controller/AttentePartieController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Equipe;
+use Doctrine\ORM\Mapping\Id;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +15,14 @@ class AttentePartieController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('attente_partie/index.html.twig', [
+        $em = $this->getDoctrine()->getManager();
+        //$MembresEquipe = $em->getRepository(Equipe::class)->find();
+        return $this->render('attente_partie/index.html.twig',
+       //array( 'equipe' => $MembresEquipe ,
+       [
             'controller_name' => 'AttentePartieController',
-        ]);
+        ])
+   // )
+    ;
     }
 }
